6a51f62ba71a811f1f16c59925907b64489b0181
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-06-03 10:37:51 +0700

    add Abstraksiklas with private members and setXY method for value assignment

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class Abstraksiklas {
+private: string x, y;
+
+public:
+
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+
+};
