e3113cd88b1a49dedefa406a6473e37a6828f316
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-06-03 10:05:28 +0700

    refactor class structure: rename 'orang' to 'seseorang' and update inheritance

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class orang {
+class seseorang {
 public:
     virtual void pesan() {
         cout << "Orang membuat pesan." << endl;
     }
 };
 
+class joko : public seseorang {
+public:
+    void pesan() override {
+        cout << "Pesan dari joko." << endl;
+    }
+};
